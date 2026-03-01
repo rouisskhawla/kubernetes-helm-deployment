aa44085ae5ef671570c2daa1cb9e2ccaf1d3163c
Khawla Rouis <38538299+rouisskhawla@users.noreply.github.com>
2026-03-01 12:01:43 +0000

    fix compilation error

--- api-gateway/src/main/java/com/devops/microservices/api/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/devops/microservices/api/gateway/config/GatewayConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class GatewayConfig {
