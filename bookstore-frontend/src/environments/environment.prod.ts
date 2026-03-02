cbf186df5e2d699ffefed305e1b59778a9672b96
Khawla Rouis <38538299+rouisskhawla@users.noreply.github.com>
2026-03-02 11:39:36 +0000

    update api url

--- bookstore-frontend/src/environments/environment.prod.ts
+++ bookstore-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api'
+  apiUrl: 'http://api-prod.bookstore.com:30860/api'
 };
--- bookstore-frontend/src/environments/environment.ts
+++ bookstore-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/api'
+  apiUrl: 'http://api-dev.bookstore.com:30860/api'
 };
